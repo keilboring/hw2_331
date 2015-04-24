e84bbcc85b593595965cf1746259c0e4c104969d
keil <keilbroing@hotmail.com>
2015-04-23 18:14:23 -0700

    AI will choose a random play

--- HumanPlayer.cpp
+++ HumanPlayer.cpp
@@ -9,11 +9,20 @@ HumanPlayer::~HumanPlayer() {
 
 }
 
-void HumanPlayer::get_move(OthelloBoard* b, int& col, int& row) {
+void HumanPlayer::get_move(OthelloBoard *b, int& col, int& row) {
     std::cout << "Enter col: ";
-    std::cin >> col;
+    //std::cin >> col;
     std::cout << "Enter row: ";
-    std::cin >> row;
+   // std::cin >> row;
+	for (int i = 0; i < 4; i++){
+		for (int x = 0; x < 4; x++){
+			if (b->is_legal_move(i, x, 'X')){
+				//printf("c:%d r:%d \n", i, x);
+				col = i;
+				row = x;
+			}
+		}
+	}
 }
 
 HumanPlayer* HumanPlayer::clone() {
--- MinimaxPlayer.cpp
+++ MinimaxPlayer.cpp
@@ -7,6 +7,9 @@
 #include <iostream>
 #include <assert.h>
 #include "MinimaxPlayer.h"
+#include <list>
+#include <unordered_map>
+#include <algorithm>>
 
 using std::vector;
 
@@ -19,8 +22,100 @@ MinimaxPlayer::~MinimaxPlayer() {
 
 }
 
-void MinimaxPlayer::get_move(OthelloBoard* b, int& col, int& row) {
+
+int MiniMaxDecision(OthelloBoard *b, int& col, int&row){
+
+	//std::list<OthelloBoard> succesors;
+
+	//for (int i = 0; i < 4; i++){
+	//	for (int x = 0; x < 4; x++){
+	//		if (b->is_legal_move(i, x, 'O')){
+	//			succesors.push_back()
+	//			printf("c:%d r:%d \n", i, x);
+	//			col = i;
+	//			row = x;
+	//			OthelloBoard *c = new OthelloBoard(*b);
+	//			c->play_move(col, row, 'O');
+	//		}
+	//	}
+	//}
+	return 0;
+}
+//int MIN_VALUE(std::unordered_map<char, node> hashTable, char state);
+//
+//int MAX_VALUE(std::unordered_map<char, node> hashTable, char state){
+//	node cur_state = hashTable[state];
+//	if (cur_state.succesors.empty()){
+//		return cur_state.utility_value;
+//	}
+//	int v = -9999;
+//	//for each successor states
+//	for (std::list<char>::iterator it = cur_state.succesors.begin(); it != cur_state.succesors.end(); it++){
+//		char temp = *it;
+//		v = std::max(v, MIN_VALUE(hashTable, temp));
+//	}
+//	return v;
+//}
+
+//int MIN_VALUE(std::unordered_map<char, node> hashTable, char state){
+//	node cur_state = hashTable[state];
+//	if (cur_state.succesors.empty()){
+//		return cur_state.utility_value;
+//	}
+//	int v = 9999;
+//	//for each successor states
+//	for (std::list<char>::iterator it = cur_state.succesors.begin(); it != cur_state.succesors.end(); it++){
+//		char temp = *it;
+//		v = std::min(v, MAX_VALUE(hashTable, temp));
+//	}
+//	return v;
+//}
+void MinimaxPlayer::get_move(OthelloBoard *b, int& col, int& row) {
     // To be filled in by you
+	while (1){
+
+
+		printf("get computer move\n");
+		for (int i = 0; i < 4; i++){
+			for (int x = 0; x < 4; x++){
+				if (b->is_legal_move(i, x, 'O')){
+
+					printf("c:%d r:%d \n", i, x);
+					col = i;
+					row = x;
+				}
+			}
+		}
+
+		OthelloBoard *c = new OthelloBoard(*b);
+		c->play_move(col, row, 'O');
+		//c->display;
+		printf("get computer move\n");
+		for (int i = 0; i < 4; i++){
+			for (int x = 0; x < 4; x++){
+				if (c->is_legal_move(i, x, 'O')){
+					printf("c:%d r:%d \n", i, x);
+					col = i;
+					row = x;
+				}
+			}
+		}
+
+		printf("get computer move\n");
+		for (int i = 0; i < 4; i++){
+			for (int x = 0; x < 4; x++){
+				if (b->is_legal_move(i, x, 'O')){
+					printf("c:%d r:%d \n", i, x);
+					col = i;
+					row = x;
+				}
+			}
+		}
+
+		getchar();
+		getchar();
+	}
+
 }
 
 MinimaxPlayer* MinimaxPlayer::clone() {
